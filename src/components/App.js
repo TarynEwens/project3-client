64a2e2ad8f7e7e5a6e652fbf39da418a4f1449af
Taryn <taryn@sugarpush.com.au>
2018-05-15 15:48:37 +1000

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react'
+// $ is a shortcut for jQuery methods
+import $ from 'jquery'
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {itemsReceived: ""}
+    this.getItems = this.getItems.bind(this)
+  }
+  getItems() {
+      let token = "Bearer " + localStorage.getItem("jwt")
+      console.log(token)
+      $.ajax({
+        url: "http://localhost:3000/api/items.json",
+        type: "GET",
+        beforeSend: function(xhr){xhr.setRequestHeader('Authorization', token)},
+        context: this, // Allows us to use this.setState inside success
+        success: function (result) {
+          console.log(result)
+          this.setState({itemsReceived: JSON.stringify(result)})
+        }
+      })
+    }
+
+
+  login () {
+    const email = $("#email").val()
+    const password = $("#password").val()
+    const request = {"auth": {"email": email, "password": password}}
+    $.ajax({
+      url: "http://localhost:3000/api/user_token",
+      type: "POST",
+      data: request,
+      dataType: "json",
+      success: function (result) {
+        console.log(result)
+        localStorage.setItem("jwt", result.jwt)
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1 style={{marginTop: "20vh", marginBottom: "5vh"}}>
+          CheesePets
+        </h1>
+        <form>
+          <label htmlFor="email">Email: </label>
+          <br />
+          <input
+            name="email"
+            id="email"
+            type="email"
+          />
+          <br /><br />
+          <label htmlFor="password">Password:</label>
+          <br />
+          <input
+            name="password"
+            id="password"
+            type="password"
+          />
+          </form>
+          <br />
+          <button
+            onClick={this.login}
+          >
+              Login
+          </button>
+        <br />
+        <button
+          onClick={this.getItems}
+          style={{marginTop: "10vh"}}
+          >
+          Get Items
+        </button>
+        <p>{this.state.itemsReceived}</p>
+      </div>
+    );
+  }
+}
+export default App
